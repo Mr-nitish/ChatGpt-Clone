bae9c4668b6a69c5d1560e9075282c56e0df90dd
Mr.nitish yadav <104161398+Mr-nitish@users.noreply.github.com>
2023-02-18 14:26:03 +0530

    Update chatGpt.py

--- chatGpt.py
+++ chatGpt.py
@@ -1,1 +1,32 @@
+# Firts import this library using command pip install openai
+import openai
 
+# fill your API-Key here in the place xxxxxxxxxx
+openai.api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# this loop will let us ask questions continuously and behave like ChatGPT
+while True:
+    # Set up the model and prompt
+    model_engine = "text-davinci-003"
+   
+    #For user input command
+    command = input('\n You : ')
+
+    if 'exit' in command or 'quit' in command:
+        break
+
+    # Generating response here
+    completion = openai.Completion.create(
+        engine=model_engine,
+        command=command,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # extracting useful part of response
+    response = completion.choices[0].text
+    
+    # printing response
+    print(response)
